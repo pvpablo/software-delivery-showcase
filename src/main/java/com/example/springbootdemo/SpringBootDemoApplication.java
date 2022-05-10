57a3ead7cf726b162c1f1de2446a1ddb8c6719fa
pvpablo <pablopv@google.com>
2022-05-10 10:39:36 -0500

    adding new version to the hello world message

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World v1.2";
+		return "Hello Docker World v1.3";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
